da0dbfc6925d30bb5e2259615ba674f2935c7267
alexggg99 <alexggg99gmail.com>
2015-10-16 17:37:05 +0300

    races add

---
+++ ocr-races/src/main/java/com/alexggg99/spring/cloud/OcrRacesApplication.java
@@ -0,0 +1,12 @@
+package com.alexggg99.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrRacesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcrRacesApplication.class, args);
+    }
+}
